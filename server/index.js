770467160d6ddabc6f8a33a61b357e6d8411ea1e
Harshit <hargup3438@gmail.com>
2025-02-17 02:01:42 +0530

    add vercel.json

--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ connectDB();
 
 app.use('/api', routes);
 
+app.get("/", (req,res)=> {
+  res.send("hello")
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
